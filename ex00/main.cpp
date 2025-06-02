1ba32500d736fe282462cd8dbf396e5df76a5dab
togauthi <togauthier23@gmail.com>
2025-06-02 16:19:20 +0200

    Maybe like that

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,25 +1,7 @@
 #include <iostream>
 #include <cstring>
+#include "whatever.hpp"
 
-template <typename T>
-void swap(T &first, T &second)
-{
-	T tmp = first;
-	first = second;
-	second = tmp;
-}
-
-template <typename T>
-T &min(T &first, T &second)
-{
-	return first < second ? first : second;
-}
-
-template <typename T>
-T &max(T &first, T &second)
-{
-	return first > second ? first : second;
-}
 
 
 int main( void ) {
