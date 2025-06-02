8254ba7a347d630daeef73dfaeac3774397f2d25
togauthi <togauthier23@gmail.com>
2025-06-02 16:10:23 +0200

    Ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+template <typename T>
+void swap(T &first, T &second)
+{
+	T tmp = first;
+	first = second;
+	second = tmp;
+}
+
+template <typename T>
+T &min(T &first, T &second)
+{
+	return first < second ? first : second;
+}
+
+template <typename T>
+T &max(T &first, T &second)
+{
+	return first > second ? first : second;
+}
+
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
