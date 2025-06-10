337bca2cb1c11dadcb27326c70bff9f18a3cc57a
togauthi <togauthier23@gmail.com>
2025-06-10 11:39:19 +0200

    Hmm some suspicious stuff in here

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,20 +3,27 @@
 #include <iostream>
 int main(void)
 {
-	Array<int> array = Array<int>(5);
+	Array<int> *array = new Array<int>(5);
 
-	std::cout << "Size is " << array.size() << std::endl;
+	std::cout << "Size is " << array->size() << std::endl;
 	
 	try {
-		array[4] = 17;
-		std::cout << "Here I put " << array[4] << std::endl;
-		std::cout << "This is the size: " << array.size() << std::endl;
-		Array<int> cpy =array;
-		cpy[4] = 1;
-		std::cout << "Hello " << array[4] << std::endl;
-		array[10] = 1;
+		(*array)[4] = 17;
+		std::cout << "Here I put " << (*array)[4] << std::endl;
+		std::cout << "This is the size: " << array->size() << std::endl;
+		// Array<int> cpy =array;
+		// cpy[4] = 1;
+		std::cout << "Hello " << (*array)[4] << std::endl;
+		(*array)[10] = 1;
 	} catch(std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 	}
+	std::cout << "Here" << std::endl;
+	int *a = new int();
+	(void) a;
+	// There is a trick with this
+	// std::cout << *array << std::endl;
+	delete a;
+	delete array;
 }
